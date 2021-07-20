dffa14c0245f6282b28c62b364becd23b4918ef7
Tim Cecatka <tim.cecatka@gmail.com>
2021-07-20 12:48:17 +0200

    server.js serving index.html with threeJS running

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const path = require('path')
 const methodOverride = require('method-override')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -12,8 +13,14 @@ mongoose.connection.on('error', error => console.error(error))
 mongoose.connection.once('open', () => console.log('Database connection established'))
 
 app.set('view engine', 'ejs')
+app.set('views', __dirname + '/frontend')
 
+app.use(express.static('frontend'))
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/frontend/index.html'))
+})
 
 app.listen(process.env.PORT || 3000)
